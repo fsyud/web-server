c21006e71e8f199c22acc08ea6ee708d2df56cc8
dongyu <naze.dong@soundws.com>
2021-06-24 14:32:34 +0800

    feat: 更新登录接口

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LoginController } from './login.controller';
+import { Login, LoginSchema } from './login.schema';
 import { LoginService } from './login.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Login.name, schema: LoginSchema }]),
+  ],
   controllers: [LoginController],
   providers: [LoginService],
 })
--- src/login/login.schema.ts
+++ src/login/login.schema.ts
@@ -4,5 +4,15 @@ import { Document } from 'mongoose';
 export type HomeDocument = Login & Document;
 
 @Schema()
+export class Login extends Document {
+  @Prop({ required: true })
+  name: string;
 
+  @Prop({ required: true })
+  password: string;
 
+  @Prop()
+  type: string;
+}
+
+export const LoginSchema = SchemaFactory.createForClass(Login);
