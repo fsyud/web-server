2be7fd1a54e66527fa1033b000e759842928553d
NaZe <dy179912099@163.com>
2021-08-22 17:07:40 +0800

    feat: 评论接口

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Comment, CommentSchema } from './comment.schema';
 import { CommentController } from './comment.controller';
 import { Home, HomeSchema } from './../home/home.schema';
+import { Auth, AuthSchema } from './../auth/auth.schema';
 import { CommentService } from './comment.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
     MongooseModule.forFeature([{ name: Home.name, schema: HomeSchema }]),
+    MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
   ],
   controllers: [CommentController],
   providers: [CommentService],
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Comment } from './comment.schema';
 import { Home } from './../home/home.schema';
+import { Auth } from './../auth/auth.schema';
 import { CommentPostDto } from './comment.dto';
 
 @Injectable()
@@ -11,6 +12,7 @@ export class CommentService {
   constructor(
     @InjectModel(Comment.name) private readonly commentModel: Model<Comment>,
     @InjectModel(Home.name) private readonly homeModel: Model<Home>,
+    @InjectModel(Auth.name) private readonly authModel: Model<Auth>,
   ) {}
 
   async getCommentList(getCommit: { article_id: string }): Promise<any> {
@@ -23,7 +25,9 @@ export class CommentService {
   }
 
   async addOneComment(commentPost: CommentPostDto): Promise<any> {
-    const { article_id } = commentPost;
+    const { article_id, user_id } = commentPost;
+
+    console.log(commentPost);
 
     const midCreate: any = {
       ...commentPost,
@@ -35,9 +39,11 @@ export class CommentService {
     const data = await this.homeModel.findById(article_id);
 
     if (data) {
-      const {
-        author_user_info: { username, avator_url, _id, type = 1 },
-      } = data;
+      const userInfo = await this.authModel.findById(user_id);
+
+      console.log(userInfo);
+
+      const { username, avator_url, _id, type = 1 } = userInfo;
 
       midCreate.oneComment = {
         user_id: _id,
@@ -67,6 +73,7 @@ export class CommentService {
 
   async addTwoComment(commentPost: CommentPostDto): Promise<any> {
     const { article_id } = commentPost;
+    console.log(commentPost);
 
     const data = await this.homeModel.findById(article_id);
 
@@ -78,28 +85,32 @@ export class CommentService {
     };
 
     if (data) {
-      const {
-        author_user_info: { username, avator_url, _id, type = 1 },
-      } = data;
+      const userInfo = await this.authModel.findById(article_id);
 
-      midCreate.oneComment = {
-        user_id: _id,
-        user_name: username,
-        type,
-        avatar: avator_url,
-      };
+      console.log(userInfo);
 
-      const commentSave = await new this.commentModel(midCreate).save();
+      // const {
+      //   author_user_info: { username, avator_url, _id, type = 1 },
+      // } = data;
 
-      if (commentSave) {
-        await this.homeModel.findByIdAndUpdate(article_id, {
-          meta: {
-            comments: data.meta.comments + 1,
-            views: data.meta.views,
-            likes: data.meta.likes,
-          },
-        });
-      }
+      // midCreate.oneComment = {
+      //   user_id: _id,
+      //   user_name: username,
+      //   type,
+      //   avatar: avator_url,
+      // };
+
+      // const commentSave = await new this.commentModel(midCreate).save();
+
+      // if (commentSave) {
+      //   await this.homeModel.findByIdAndUpdate(article_id, {
+      //     meta: {
+      //       comments: data.meta.comments + 1,
+      //       views: data.meta.views,
+      //       likes: data.meta.likes,
+      //     },
+      //   });
+      // }
     }
 
     return {
